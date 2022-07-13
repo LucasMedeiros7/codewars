437fcf6285b185672e6d4aab4d1bc03772d7624f
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-07-13 16:51:16 -0300

    1/100

---
+++ 8 kyu/EvenOrOdd.js
@@ -0,0 +1,8 @@
+// [8 kyu] Even or Odd?
+
+// Description:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// Solution:
+const even_or_odd = number => number % 2 === 0 ? 'Even' : 'Odd';
+// :)
