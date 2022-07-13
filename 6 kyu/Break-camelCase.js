33d31de582cfbafa794db2c781e52ac49cdc8ca2
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-07-13 19:19:18 -0300

    atualizando readme

--- 6 kyu/Break-camelCase.js
+++ 6 kyu/Break-camelCase.js
@@ -7,16 +7,12 @@
 // solution("camelCasing") //should return "camel Casing"
 // solution("camelCasingTest") //should return "camel Casing Test"
 
-
-// My Solution:
-
+// Solution:
 function solution(string) {
   return string
     .split('')
     .map(item => (/[A-Z]/g).test(item) ? ` ${item}` : item)
     .join('');
 }
-
-// Other Solutions:
-
+// Other solution:
 const simpleSolution = string => string.replace(/([A-Z])/g, ' $&');
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # codewars
+
+<img src="https://www.codewars.com/users/LucasMedeiros7/badges/small"/>
+
 Repositório para organizar minhas soluções dos desafios da plataforma codewars
